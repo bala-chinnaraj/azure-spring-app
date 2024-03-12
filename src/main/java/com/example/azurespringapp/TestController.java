3b730dc2f51e009f28bc14d05635d58c0d4b6e97
bala-chinnaraj <bala032017@gmail.com>
2024-03-12 13:12:17 -0400

    First commit

---
+++ src/main/java/com/example/azurespringapp/TestController.java
@@ -0,0 +1,16 @@
+package com.example.azurespringapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String getMethodName() {
+        return "Welcome to Azure";
+    }
+    
+}
---
+++ src/test/java/com/example/azurespringapp/AzureSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
